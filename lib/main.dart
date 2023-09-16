0bca9e3dd091376f2dc3542d130be1e9a46d0f4a
hongjyoun <hongjyoun@gmail.com>
2023-09-16 19:52:30 +0900

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:app_one/firebase_options.dart';
 import 'package:app_one/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(const GameOneApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 class GameOneApp extends StatelessWidget {
